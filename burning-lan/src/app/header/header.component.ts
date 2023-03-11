c2dc385c26ed73c621eddce2558bd450d4b478ff
Gabriel Leite <gabrielholeite@gmail.com>
2023-03-11 13:28:52 +0000

    Create navbar and footer

--- burning-lan/src/app/header/header.component.ts
+++ burning-lan/src/app/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('menuButton')
+  menuButton!: ElementRef;
 
-  constructor() { }
+  isMenuVisible = false;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleMenu(): void {
+    this.isMenuVisible = !this.isMenuVisible;
+    if (!this.isMenuVisible) {
+      this.menuButton.nativeElement.classList.remove('active');
+    }
   }
-
 }
